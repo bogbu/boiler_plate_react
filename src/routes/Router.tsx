da2828ab61f398aa526b80226313ad0b8578bdb0
bogbu <glutamine082442@gmail.com>
2023-10-16 02:38:56 +0900

    add page path

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,7 +1,7 @@
 import {Route, Routes} from "react-router-dom";
 import Layout from "../layout/Layout.tsx";
-import Home from "../pages/home.tsx";
-import About from "../pages/About.tsx";
+import Home from "../pages/Home/home.tsx";
+import About from "../pages/About/About.tsx";
 
 const Router = () => {
     return (
